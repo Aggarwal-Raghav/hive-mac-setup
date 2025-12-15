25c4303850657d4d1a05cbefc7ab7e86dc2d2e8a
Raghav Aggarwal <raghavaggarwal03.ra@gmail.com>
2025-12-15 14:23:51 +0530

    Add hdfs service start and stop script

---
+++ hadoop/bin/start.sh
@@ -0,0 +1,7 @@
+./hdfs --daemon start namenode
+./hdfs --daemon start datanode
+./hdfs --daemon start secondarynamenode
+./yarn --daemon start resourcemanager
+./yarn --daemon start nodemanager
+./yarn --daemon start timelineserver
+./mapred --daemon start historyserver
---
+++ hadoop/bin/stop.sh
@@ -0,0 +1,7 @@
+./hdfs --daemon stop namenode
+./hdfs --daemon stop datanode
+./hdfs --daemon stop secondarynamenode
+./yarn --daemon stop resourcemanager
+./yarn --daemon stop nodemanager
+./yarn --daemon stop timelineserver
+./mapred --daemon stop historyserver
